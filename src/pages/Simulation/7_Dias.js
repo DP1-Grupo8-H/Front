f4161d93ab115f37bef58bf8285fb6c02023dd5c
Daniel Marquina Inca <a20180191@pucp.edu.pe>
2022-06-06 23:16:37 -0500

    asdas
    asdas

--- src/pages/Simulation/7_Dias.js
+++ src/pages/Simulation/7_Dias.js
@@ -2,6 +2,13 @@ import React, {useState, useEffect} from "react";
 import Prueba from './Prueba';
 import './App.css';
 const Seve_Days = () => {
+<<<<<<< Updated upstream
+=======
+  const [openPopup, setOpenPopup] = useState(false);
+  const [data, setData] = useState([]);
+
+  console.log(data);
+>>>>>>> Stashed changes
   return(
     <div className="App">
       <header className="App-header">
