f7f733b6556ca963ded519569b06c085045e5e0d
Daniel Marquina Inca <a20180191@pucp.edu.pe>
2022-06-16 20:58:17 -0500

    Nueva version gozu
    gaa

--- src/services/algoritmoService.js
+++ src/services/algoritmoService.js
@@ -6,12 +6,12 @@ const url = 'http://localhost:8080';
 
 const testAlgorithm = async (data) => {
   for(const d of data){
-    d.fecha_registro = format(d.fecha_registro, 'yyyy-MM-dd kk:mm:ss')
-    d.fecha_entrega_max = format(d.fecha_entrega_max, 'yyyy-MM-dd kk:mm:ss')
+    d.fecha_registro = format(d.fecha_registro, 'yyyy-MM-dd hh:mm:ss')
+    d.fecha_entrega_max = format(d.fecha_entrega_max, 'yyyy-MM-dd hh:mm:ss')
   }
   try{
     const request = await axios.post(`${url}/algoritmo/simulacion/`, data);
-
+    console.log(request.data);
     return request.data;  
   }catch(exception){
     console.error(exception);
